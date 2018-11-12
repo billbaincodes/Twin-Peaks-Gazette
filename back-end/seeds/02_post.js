159df6253940929c13301325406c8a5460f6e749
bill <williamandrewbain@gmail.com>
2018-11-12 13:43:10 -0700

    added head and body to post compoonent

--- back-end/seeds/02_post.js
+++ back-end/seeds/02_post.js
@@ -8,21 +8,21 @@ exports.seed = function(knex, Promise) {
           id: 1,
           title: "weird peeps",
           body: "anyone else seeing weird people around",
-          imageURL: "https://picsum.photos/200",
+          imageURL: "https://m.media-amazon.com/images/M/MV5BNWU0ZWNiYmUtYjhhZC00Y2E4LTg1MWUtZmZmMGNhMTM2M2YzXkEyXkFqcGdeQXVyMjYyODQ0OTQ@._V1_.jpg",
           character_id: 3
         },
         {
           id: 2,
           title: "pay attention to me",
           body: "im audrey horne and i need more attention on me than stupid laura",
-          imageURL: "https://picsum.photos/200",
+          imageURL: "https://m.media-amazon.com/images/M/MV5BMTM5Mzg1MjUyOV5BMl5BanBnXkFtZTgwMTU1Mjg0MDE@._V1_SY1000_CR0,0,686,1000_AL_.jpg",
           character_id: 2
         },
         {
           id: 3,
           title: "ivy consumption",
           body: "does eat oats and and oats and little lambs eat ivy",
-          imageURL: "https://picsum.photos/200",
+          imageURL: "https://m.media-amazon.com/images/M/MV5BMTA3MTE3OTQ0OTBeQTJeQWpwZ15BbWU4MDAzMjQ5NDAx._V1_SY1000_CR0,0,1481,1000_AL_.jpg",
           character_id: 1
         }
       ])
--- twin-peaks-front/src/components/Post.js
+++ twin-peaks-front/src/components/Post.js
@@ -3,9 +3,15 @@ import React from 'react'
 const Post = ({ post }) => {
   return(
     <article className="post">
-      <h3>{post.title}</h3>
-      <img src={post.imageURL} />
-      <p>{post.body}</p>
+      <header className="post-head">
+        <h4>{post.name}</h4>
+        <img src={post.avatarURL} alt="post" />
+      </header>
+      <main className="post-body">
+        <h3>{post.title}</h3>
+        <img src={post.imageURL} alt="post" />
+        <p>{post.body}</p>
+      </main>
     </article>
   )
 }
